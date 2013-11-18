f8018cbe2b28d3bddc4748905d0948df23de375c
Aldric Giacomoni <trevoke@gmail.com>
2013-11-17 21:41:32 -0500

    face-finding function, cask cleanup, my color theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -90,7 +90,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("bb848b287a4e3162d63141cbacc64949e7dbeb41c944a0ba040785c285f5c9ba" "47583b577fb062aeb89d3c45689a4f2646b7ebcb02e6cb2d5f6e2790afb91a18" "847c431bd5eb5017b601f4c599fb4b23b096e48739e4a708f57ac756358751f0" "5c674d297206a2494eff9bf650a2ffbb8261d5a2ee77563b8a6530525fec5b6d" "f89e21c3aef10d2825f2f079962c2237cd9a45f4dc1958091be8a6f5b69bb70c" "8020f88a6175dc3c79d53072c8c677a14a3d24fa165b740995bace5870ae9157" default))))
+ '(custom-safe-themes
+   (quote
+    ("0be7333e1d97d55b042f1e28c1b50b870ccc344c8af61bbd6a860e1c8ad9f883" "78eb32ca2bb2c2b71f3ea218d0d7d4260bdb50e64bc1e50451a310ddef48201b" "b2cf3c76acf10bd05cfcd7d3f924f4616c1504484de96be474594c62d59037ba" "70ed2f3b5419fdfd7012d1594773b92c0c0eba5a912a22624fc645f846c1118a" "74c5e4f755ed3d75c7ce9f3f08ea12124b01bbf513a551a40366f65c5d4a8a7a" "bb848b287a4e3162d63141cbacc64949e7dbeb41c944a0ba040785c285f5c9ba" "47583b577fb062aeb89d3c45689a4f2646b7ebcb02e6cb2d5f6e2790afb91a18" "847c431bd5eb5017b601f4c599fb4b23b096e48739e4a708f57ac756358751f0" "5c674d297206a2494eff9bf650a2ffbb8261d5a2ee77563b8a6530525fec5b6d" "f89e21c3aef10d2825f2f079962c2237cd9a45f4dc1958091be8a6f5b69bb70c" "8020f88a6175dc3c79d53072c8c677a14a3d24fa165b740995bace5870ae9157" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ .emacs.d/themes/mejelly-theme.el
@@ -0,0 +1,1 @@
+color-theme-mejelly/mejelly-theme.el
