c35397b1f2a2042cd70318a84a9d91145bf260a3
Aldric Giacomoni <trevoke@gmail.com>
2013-12-10 22:02:43 -0500

    move custom stuff, add emacs-lisp hooks

--- .emacs.d/init.d/elisp.el
+++ .emacs.d/init.d/elisp.el
@@ -1,4 +1,7 @@
 (add-hook 'lisp-mode-hook 'stag-code-modes-hook)
-
 (add-hook 'lisp-mode-hook 'paredit-mode)
 (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
+
+(add-hook 'emacs-lisp-mode-hook 'stag-code-modes-hook)
+(add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,6 +23,10 @@
 (setq user-emacs-directory stag-emacs-config-dir)
 (setq stag-init-dir (concat stag-emacs-config-dir  "init.d"))
 
+;; The Customize stuff is annoying in this file. Let's put it somewhere else.
+(setq custom-file
+      (concat stag-init-dir "init-custom.el"))
+
 (if (file-exists-p stag-init-dir)
     (dolist (file (directory-files stag-init-dir t "\.el$"))
       (load file)))
@@ -78,21 +82,3 @@
 
 (prefer-coding-system 'utf-8)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(custom-safe-themes
-   (quote
-    ("0ddfa62c1bcd60434cf317f68ea858f69000aaee67cb563e1d15774df2b05a6a" "a8ea675d92abfa2a5c35a0d982474ad3767079cc58b868572451436368229504" "c0f1925e602e5487d775cec511466e2c3cdd3724e6263fb048a222db6e5deaf0" "fe77c7b060c61b467514796585d913ef91fb33dd745c4a9e8e876c6ca59c4314" "2af9b229248e2a0ffd959292d73e082d5741fb565abe93e4e27b3f022270198c" "c70e6b9435886beb93dc4eae4de502b59fa9bab992ec0c56bad80da219d110a4" "a77b40f749e8c3aaa4172c298e694eafe0030511470ff3406201c3915b811fa7" "67ccaa34f304fecf5b7e5a078f9b7106b0c3b8bc8ec83803d2cad935be875531" default)))
- '(line-number-mode nil)
- '(show-paren-mode t)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
