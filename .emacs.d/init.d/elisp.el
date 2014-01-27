d09c2ff81dec70358de7227b08fbe52fcb872ccb
Aldric Giacomoni <trevoke@gmail.com>
2014-01-26 20:40:18 -0500

    turn on eldoc mode for extra LISP fun

--- .emacs.d/init.d/elisp.el
+++ .emacs.d/init.d/elisp.el
@@ -5,3 +5,7 @@
 (add-hook 'emacs-lisp-mode-hook 'stag-code-modes-hook)
 (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
+
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
