a199ca2bdd0112e79582c38650adf143cd39e901
Aldric Giacomoni <trevoke@gmail.com>
2014-02-15 16:15:24 -0500

    remove rainbow-delimiters

--- .emacs.d/init.d/elisp.el
+++ .emacs.d/init.d/elisp.el
@@ -1,10 +1,8 @@
 (add-hook 'lisp-mode-hook 'stag-code-modes-hook)
 (add-hook 'lisp-mode-hook 'paredit-mode)
-(add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
 
 (add-hook 'emacs-lisp-mode-hook 'stag-code-modes-hook)
 (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
-(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
