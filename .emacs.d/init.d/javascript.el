e2b295c2d66251288fa646c959519eadeb6d9fb6
Aldric Giacomoni <trevoke@gmail.com>
2014-06-16 22:53:52 -0400

    How about JS3-mode then?

--- .emacs.d/init.d/javascript.el
+++ .emacs.d/init.d/javascript.el
@@ -1,2 +1,2 @@
-(add-to-list 'auto-mode-alist '(".js$" . js2-mode))
-(add-hook 'js2-mode-hook 'stag-code-modes-hook)
+(add-to-list 'auto-mode-alist '(".js$" . js3-mode))
+(add-hook 'js3-mode-hook 'stag-code-modes-hook)
